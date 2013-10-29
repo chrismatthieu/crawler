828de6a15e81724c216215b802f3c7db8f6a42b6
Chris Matthieu <chris@matthieu.us>
2013-10-29 08:22:20 -0700

    added regex to look for mentions of our name on a site

--- server.js
+++ server.js
@@ -8,8 +8,15 @@ var c = new Crawler({
     // console.log(error);
     // console.log(result);
     // console.log($);
+    // console.log(result.body);
 
-    console.log(result.body);
+    if(result){
+      var page = result.body; 
+      var res = page.match(/bechtel/i);
+      if (res && res.length > 0){
+        console.log(result.body);
+      }
+    }
 
     // $ is a jQuery instance scoped to the server-side DOM of the page
     $("a").each(function(index,a) {
@@ -20,4 +27,4 @@ var c = new Crawler({
 });
 
 // Queue just one URL, with default callback
-c.queue("http://bechtel.com");
+c.queue("http://construction.com");
